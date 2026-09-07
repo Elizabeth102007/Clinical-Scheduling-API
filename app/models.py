49f90610ea2adf3924c2777f3dab5a6cf24b741c
Elizabeth102007 <drjenniferelizabeth@gmail.com>
2026-09-07 19:27:01 +0300

    Made a few changes

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
+from enum import Enum
 from sqlmodel import SQLModel, Field, Column, DateTime
 from typing import Optional
 from datetime import datetime, timezone
-
-
+from sqlalchemy import Enum as SAEnum
+# timezone helper
 def get_utc_now() -> datetime:
     return datetime.now(timezone.utc)
 
@@ -15,6 +16,15 @@ class Patient(SQLModel, table=True):
     created_at: datetime = Field(default_factory=get_utc_now,
                                  sa_column=Column(DateTime(timezone=True), nullable=False))
 
+class StatusEnum(str, Enum):
+    SCH = "scheduled"
+    COM = "completed"
+    CAN = "cancelled"
+
+# enum helper
+def enum_values(enum_cls):
+    return[member.value for member in enum_cls]
+
 class Appointment(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     patient_id: int = Field(foreign_key="patient.id")
@@ -23,7 +33,11 @@ class Appointment(SQLModel, table=True):
         sa_column=Column(DateTime(timezone=True), nullable=False)
     )
     reason: str
-    status: str = Field(default="scheduled")
+    status: StatusEnum = Field(
+    default=StatusEnum.SCH,
+    sa_column=Column(SAEnum(StatusEnum, values_callable=enum_values, name="status_enum"),
+        nullable=False)
+    )
     created_at: datetime = Field(
         default_factory=get_utc_now,
         sa_column=Column(DateTime(timezone=True), nullable=False)
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,8 @@
+
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
-
+from app.models import StatusEnum
 class PatientBase(SQLModel):
     full_name: str = Field(min_length=2, max_length=130)
     age: int = Field(gt=0)
@@ -16,7 +17,7 @@ class PatientRead(PatientBase):
      created_at: datetime
 
 class PatientUpdate(SQLModel):
-     full_name: Optional[str] = Field(default=None, min_length=2, max_length=130)
+     full_name: Optional[str] = Field(default=None,min_length=2, max_length=130)
      age: Optional[int] = Field(default=None, gt=0)
      diagnosis: Optional[str]= None
      is_active: Optional[bool] = None
@@ -28,15 +29,15 @@ class AppointmentBase(SQLModel):
      reason: str
 
 class AppointmentCreate(AppointmentBase):
-     pass
+     status:  StatusEnum = StatusEnum.SCH
 
 class AppointmentRead(AppointmentBase):
      id: int
-     status: str = Field(default="scheduled")
+     status: StatusEnum 
      created_at : datetime
 
 class AppointmentUpdate(SQLModel):
-     doctor_name: Optional[str] = Field(min_length=2, max_length=140)
-     reason: Optional[str]
+     doctor_name: Optional[str] = Field(default=None, min_length=2, max_length=140)
+     reason: Optional[str] = None
      appointment_time: Optional[datetime] = None
-     status: Optional[str] = Field(default="scheduled")
+     status: Optional[StatusEnum] = None
